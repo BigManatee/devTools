8db7c417765d80588bdd667974991bc71f7fd86e
Tyler J <tylerjc@hotmail.co.uk>
2018-04-22 12:51:22 +0100

    fixed nav links

--- assets/inc/nav.php
+++ assets/inc/nav.php
@@ -2,10 +2,10 @@
     <div class="panel-standard menu-standard">
         <span class="title-grey">menu</span>
         <ul>
-            <li><a href="/Tools/"><i class="fa fa-home"></i> Home</a></li>
-            <li><a href="/Tools/charactercount"><i class="fa fa-adn"></i> Character Count</a></li>
-            <li><a href="/Tools/base64"><i class="fa fa-wrench"></i> Base64 En/Decode</a></li>
-            <li><a href="/Tools/duplicatelinecounter"><i class="fa fa-list-ul"></i> Duplicate Line Counter</a></li>
+            <li><a href="/"><i class="fa fa-home"></i> Home</a></li>
+            <li><a href="/charactercount"><i class="fa fa-adn"></i> Character Count</a></li>
+            <li><a href="/base64"><i class="fa fa-wrench"></i> Base64 En/Decode</a></li>
+            <li><a href="/duplicatelinecounter"><i class="fa fa-list-ul"></i> Duplicate Line Counter</a></li>
         </ul>
     </div>
 </div>
